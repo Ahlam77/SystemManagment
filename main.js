f75b2d9497fec19f92c89c153022c74c1cf65500
ahlam alshaba <ama1977ln@outlook.com>
2022-01-28 13:12:35 +0100

    creating System Mangment

---
+++ main.js
@@ -0,0 +1,111 @@
+//do do list
+// get total
+// creat product 
+// save localStorage
+// clear inputs 
+// read
+// count 
+// delete 
+// update 
+// search 
+// clean date 
+
+
+let title = document.getElementById(`title`);
+let price = document.getElementById(`price`);
+let taxes = document.getElementById(`taxes`);
+let ads = document.getElementById(`ads`);
+let discount = document.getElementById(`discount`);
+let total = document.getElementById(`total`);
+let count = document.getElementById(`count`);
+let category = document.getElementById(`category`);
+let submit = document.getElementById(`submit`);
+
+console.log(taxes, title, price, ads, discount, total,count, category, submit );
+// get total
+
+function getTotal (){
+    if (price.value != ``){
+    let result = (+price.value + +taxes.value + +ads.value) - +discount.value;
+    console.log(`done`);
+total.innerHTML = result;
+total.style.background = `#040`;
+    }else{
+total.innerHTML = ` `;
+total.style.background = `#a00d02`;
+    }
+}
+// creat product
+let dataPro;
+if(localStorage.product != null){
+    dataPro= JSON.parse(localStorage.product)
+}else{
+    dataPro = [];
+}
+
+submit.onclick = function (){
+    let newPro = {
+title:title.value,
+price:price.value,
+taxes:taxes.value,
+ads:ads.value,
+discount:discount.value,
+total:total.innerHTML,
+count:count.value,
+category:category.value,
+
+    }
+    // save localStorage
+    dataPro.push(newPro);
+    localStorage.setItem(`product`,            JSON.stringify(dataPro))
+    console.log(dataPro)
+    clearData()
+    showData()
+}
+
+// clean date 
+function clearData(){
+title.value= "";
+price.value ="";
+taxes.value ="";
+ads.value= "";
+discount.value ="";
+total.innerHTML="";
+count.value= "";
+category.value= "";
+
+}
+// read
+function showData(){
+let table = " ";
+for (let i = 0; i< dataPro.length; i++){
+table += `
+
+<tr>
+              <td>${i}</td>
+              <td>${dataPro[i].title}</td>
+              <td>${dataPro[i].price}</td>
+              <td>${dataPro[i].taxes}</td>
+              <td>${dataPro[i].ads}</td>
+              <td>${dataPro[i].discount}</td>
+               <td>${dataPro[i].total}</td> 
+              <td>${dataPro[i].category}</td>
+              <td><button id="update">update</button></td>
+              <td><button onclick= "deleteData(${i}) "   id="delete">delete</button></td>
+            </tr>
+
+`
+}
+document.getElementById(`tbody`).innerHTML = table;
+}
+
+showData()
+
+// delete 
+
+function deleteData(i){
+dataPro.splice(i,1);
+localStorage.product= JSON.stringify(dataPro);
+showData()
+}
+
